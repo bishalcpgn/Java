2522387f24bed53df332791c4ac3ef8868f08d49
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-07 09:40:57 +0545

    Hello World !

---
+++ Hello.java
@@ -0,0 +1,15 @@
+// To print 'Hello World !' in java 
+
+class HelloWorld{
+    public static void main(String[] args){   
+             
+        System.out.println("Hello World !");
+    }
+}
+
+/* 
+
+Here, class name and file name are different. 
+The class name must match with file name only when class is declared as public.
+
+*/

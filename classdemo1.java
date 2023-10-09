6188ae0acbd6d2a935d72a617f2e9e569fcd22f2
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-09 18:39:48 +0545

    class and object demo

---
+++ classdemo1.java
@@ -0,0 +1,19 @@
+// A class to accept values and return their sum  
+
+public class classdemo1{
+    public static void main(String[] args){
+        int num1 = 4;
+        int num2 = 6;        
+        calculator calc = new calculator(); // Making object of class calculator
+        int result = calc.add(num1, num2);
+        System.out.println("Total sum : "+result);
+    
+    }
+}
+
+class calculator{
+    public int add(int num1, int num2){
+        int sum = num1 + num2;        
+        return sum;
+    }
+}

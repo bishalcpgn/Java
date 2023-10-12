9af9b6d6622f6304da0755551d39e80a838366e0
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-12 21:18:23 +0545

    parameterized constructor

---
+++ Constructor1.java
@@ -0,0 +1,47 @@
+/*
+
+    Check Constructor.java file for notes. 
+
+    Syntax of parameterized constuctor :
+
+         AccessModifier ClassName( parameters ) {
+
+            // assign values here 
+
+        }
+
+    For invoking :
+
+        ClassName object = new ClassName( arguments );
+
+
+
+
+*/
+
+class Calc {
+    int a, b;
+    
+    Calc(int x , int y ) {             // parameterized constructor is created here 
+        this.a = x;
+        this.b = y;  
+        System.out.println("Constructor is called : "); 
+
+    }
+
+    int add() {
+        return a + b ;
+    }
+}
+
+class Constructor1 {
+    public static void main(String[] args) {
+        
+        Calc c1 = new Calc(5, 10); // invokes constructor 
+
+        int c = c1.add();
+
+        System.out.println("The total sum is : " + c);
+    }
+}
+

09b0d08cded7c781efb53602fd59dc095bd15cb6
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-13 11:19:36 +0545

    inhertitance notes, single inheritance's demo

---
+++ inheritance.java
@@ -0,0 +1,60 @@
+/* 
+Inheritance :
+
+    1. One of the important features of OOP, where features of one class can be transferred to next one.
+
+    2. The `extends` keyword is used for inheritance in Java. 
+
+    3. Private members and constructor can't be inherited.
+
+    4. Types of inheritance :
+        a. Single Inheritance          ->     a class is derived from only one super/parent class
+        d. Multilevel Inheritance      ->     a subclass is derived from another subclass forming a chain like structure 
+        c. Multiple Inheritance        ->     a interface can be derived from multiple interfaces, Java does not support multiple inheritances with classes. 
+        d. Hierarchical Inheritance    ->     multiple subclasses can be derived from a single superclass
+        e. Hybrid Inheritance          ->     mix of two or more types of inheritance, can be achived using interfaces
+
+
+    Syntax for Single Inheritance : 
+
+        class ChildClass extends SuperClass {  
+
+            // Extra Features 
+
+        }  
+
+
+*/
+
+class Calculator {
+
+    String add(int a , int b) {
+
+        return ("Addition : "+ (a + b)) ; 
+    }
+
+}
+
+class AdvanceCalculator extends Calculator {
+
+    String sub(int a , int b) {
+
+        return ("Subtraction  : " + (a - b)) ; 
+    }
+
+}
+
+
+class inheritance {
+    public static void main(String[] args) {
+
+        AdvanceCalculator c1 = new AdvanceCalculator();
+
+        String result1 = c1.add(5,6);
+
+        String result2 = c1.sub  (5,6);
+
+        System.out.println(result1 + " " + result2);
+
+    }
+}

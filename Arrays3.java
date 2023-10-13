e33ba184fe4eb71f59aad1cff4e61d51247901e4
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-13 11:45:10 +0545

    jagged array

--- Arrays3.java
+++ Arrays3.java
@@ -1,6 +1,6 @@
 // It is a multidimensional array where each row can have a different length.
 
-class JaggedArray{
+class Arrays3{
     public static void main(String[] args){
 
         int[][] datas = new int[3][]; // two-dimensional array is created with four rows but length of rows isn't specified.
